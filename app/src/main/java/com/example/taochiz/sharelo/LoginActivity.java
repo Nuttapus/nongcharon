b93cd231961641cc92e251c869f0095f236bc132
TAOCHIZ <taochiz@icloud.com>
2018-10-28 00:13:57 +0700

    Login & Register

---
+++ app/src/main/java/com/example/taochiz/sharelo/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.taochiz.sharelo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_login);
+
+        Button bR = (Button)findViewById(R.id.btnRegister);
+        bR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/taochiz/sharelo/RegisterActivity.java
@@ -0,0 +1,25 @@
+package com.example.taochiz.sharelo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_register);
+
+        Button bR = (Button)findViewById(R.id.btnBackRegis);
+        bR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
