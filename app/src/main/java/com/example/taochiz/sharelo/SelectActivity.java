9c2f8014a087c2810d1f332bf847cc8a47a0bc3d
TAOCHIZ <taochiz@icloud.com>
2018-10-28 04:31:41 +0700

    ui finish

--- app/src/main/java/com/example/taochiz/sharelo/SelectActivity.java
+++ app/src/main/java/com/example/taochiz/sharelo/SelectActivity.java
@@ -1,7 +1,10 @@
 package com.example.taochiz.sharelo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class SelectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_select);
+
+        Button bR = (Button)findViewById(R.id.btnSearch);
+        bR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SelectActivity.this, SearchActivity.class));
+            }
+        });
+
+        Button bW = (Button)findViewById(R.id.btnWrite);
+        bW.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SelectActivity.this, WriteActivity.class));
+            }
+        });
     }
 }
