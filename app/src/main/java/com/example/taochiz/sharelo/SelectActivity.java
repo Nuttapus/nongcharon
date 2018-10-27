654b4a637aa835c4ad5cfb281521d0bf5e2c0336
TAOCHIZ <taochiz@icloud.com>
2018-10-28 01:12:49 +0700

    Select

---
+++ app/src/main/java/com/example/taochiz/sharelo/SelectActivity.java
@@ -0,0 +1,14 @@
+package com.example.taochiz.sharelo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_select);
+    }
+}
